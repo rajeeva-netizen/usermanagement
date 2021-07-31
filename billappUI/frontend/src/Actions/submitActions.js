d5b73ee61309006d5cb3901709dd37fd1b8e6e0f
rajeevabudal <rajeeva.hrithik@gmail.com>
2021-07-31 23:22:16 +0530

    added post request

--- billappUI/frontend/src/Actions/submitActions.js
+++ billappUI/frontend/src/Actions/submitActions.js
@@ -1,5 +1,5 @@
-import { SUBMIT_OK } from "./type"
-
+import { SUBMIT_OK, ADD_CUSTOMERS } from "./type"
+import axios from "axios"
 export const submit=(bool)=>{
     console.log(bool)
     return (dispatch)=>{
@@ -7,5 +7,23 @@ export const submit=(bool)=>{
             type:SUBMIT_OK,
             bool
         })
+    }
+}
+
+export const sendData=(val)=>{
+    console.log(val)
+    return (dispatch)=>{
+        axios.post('localhost:4000/newcustomer', val)
+        .then(res=>{
+            dispatch({
+                type:ADD_CUSTOMERS,
+                val
+            })
+            console.log(res)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+        
     }
 }
